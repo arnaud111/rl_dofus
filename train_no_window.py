270bb6cc62761d011cc63322b731bf192496695c
Dixen-Naxos <armanddailly@gmail.com>
2024-02-13 00:46:13 +0100

    Mpg during train no window

--- train_no_window.py
+++ train_no_window.py
@@ -1,13 +1,14 @@
+from datetime import datetime
+
 from matplotlib import pyplot as plt
 from object.dofus_agent import Agent
 from utils_dofus import REWARD_END_FIGHT
 
 agent = Agent()
-#agent.load("./results/dofus - 2024-02-09 21:01.qtable")
+# agent.load("./results/dofus - 2024-02-09 21:01.qtable")
 history = []
 agent.score = -1
-cnt = 0
-
+last_registered_time = datetime.now()
 for i in range(int(1e6)):
 
     map_state = agent.reset()
@@ -17,8 +18,9 @@ for i in range(int(1e6)):
         if reward == REWARD_END_FIGHT:
             history.append(agent.score)
             break
-
-    cnt += 1
+    if i % 1000 == 0:
+        print(f"Iterations : {i}, best score : {max(history)}, time elapsed : {datetime.now() - last_registered_time}")
+        last_registered_time = datetime.now()
 
 agent.save()
 plt.plot(history)
